9b3cdcd281e8bc643724a65a80c798965578f110
hhnssl <losnah5h@gmail.com>
2022-08-19 23:40:08 +0900

    캡슐화된 값을 getter로 가져오기, 그룹화해서 사용하는 방법

--- 모듈/main.js
+++ 모듈/main.js
@@ -1,6 +1,18 @@
-// 다른 파일에서 export 한 것을 사용하는 방법: import .. from ..
-import increase from "./counter.js"; // increase 대신에 원하는 이름으로 바꿔도 됨~
+// import { increase, getcount } from "./counter.js";
+// import { increase, getcount, count } from "./counter.js"; /// 변수도 export 하면 가져오기 가능하지만 권장 ㄴㄴ
 
-increase();
-increase();
-increase();
+// increase();
+// increase();
+// increase();
+
+// const count = getcount();
+// console.log(count);
+
+// 그룹화해서 사용하는 방법
+import * as counter from "./counter.js"; // 모든 것들을 가져와  counter라는 이름으로 사용하겠다.
+
+// 그룹화한 것 사용 방법: 점을 붙여야함
+counter.increase();
+counter.increase();
+counter.increase();
+console.log(counter.getcount());
